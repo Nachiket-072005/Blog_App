06bdd8cf5ccd9e4951f02776e1ec8ac86e099c18
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-12-12 14:20:49 +0530

    Added CRUD Operation for post and error handler

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
@@ -8,10 +8,20 @@ import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 
 const app = express();
+app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    message: error.message || "Something went wrong!",
+    status: error.status || 500,
+    stack: error.stack,
+  });
+});
 
 app.listen(3000, () => {
   connectDB();
